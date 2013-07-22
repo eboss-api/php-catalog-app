532bb1b8961fad7f9d8723466e93b665bdcd084c
Tim <tim@dodat.de>
2013-07-21 21:50:28 -0700

    set default iframe source to current downloader url

--- layout/includes/download_modal.php
+++ layout/includes/download_modal.php
@@ -1,4 +1,4 @@
 <div class="modal fade hide" id="DownloadModal" role="dialog" aria-hidden="true">
 	<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-	<iframe src="#" width="100%" height="100%" frameborder="0" seamless></iframe>
+	<iframe src="<?php echo $current_downloader_url; ?>" width="100%" height="100%" frameborder="0" seamless></iframe>
 </div>
